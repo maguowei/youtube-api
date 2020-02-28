4843feecfd5498fb7fb0eea584bb1b8f99b1e840
maguowei <imaguowei@gmail.com>
2020-02-28 17:57:45 +0800

    youtube-dl as restful

--- app.py
+++ app.py
@@ -1,29 +1,26 @@
 from chalice import Chalice
+import youtube_dl
+
 
 app = Chalice(app_name='youtube_api')
+
+dl = youtube_dl.YoutubeDL()
 
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}
+    request = app.current_request
+    query_params = request.query_params
+    
+    url = query_params.get('url') if query_params else ''
+    if url:
+        code = 0
+        data = dl.extract_info(url, download=False)
+    else:
+        code = 1
+        data = {}
 
-
-# The view function above will return {"hello": "world"}
-# whenever you make an HTTP GET request to '/'.
-#
-# Here are a few more examples:
-#
-# @app.route('/hello/{name}')
-# def hello_name(name):
-#    # '/hello/james' -> {"hello": "james"}
-#    return {'hello': name}
-#
-# @app.route('/users', methods=['POST'])
-# def create_user():
-#     # This is the JSON body the user sent in their POST request.
-#     user_as_json = app.current_request.json_body
-#     # We'll echo the json body back to the user in a 'user' key.
-#     return {'user': user_as_json}
-#
-# See the README documentation for more examples.
-#
+    return {
+        'code': code,
+        'data': data,
+    }
